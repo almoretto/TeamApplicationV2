cd34a224ba7b5ca50eefe2aa017bcade8f6c1f64
André Moretto <amoretto@outlook.com.br>
2021-01-07 08:26:12 -0300

    Migrations alterations

---
+++ TeamApplication/Migrations/20201016142722_modelv2.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace TeamApplication.Migrations
+{
+    public partial class modelv2 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Volunteer_Team_TeamId",
+                table: "Volunteer");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Volunteer_TeamId",
+                table: "Volunteer");
+
+            migrationBuilder.DropColumn(
+                name: "TeamId",
+                table: "Volunteer");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "TeamId",
+                table: "Volunteer",
+                type: "int",
+                nullable: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Volunteer_TeamId",
+                table: "Volunteer",
+                column: "TeamId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Volunteer_Team_TeamId",
+                table: "Volunteer",
+                column: "TeamId",
+                principalTable: "Team",
+                principalColumn: "TeamId",
+                onDelete: ReferentialAction.Restrict);
+        }
+    }
+}
